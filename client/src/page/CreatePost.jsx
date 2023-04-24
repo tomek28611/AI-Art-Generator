e5f1d20a2c7502a408e5093bb809c26855cb9090
Tomek28611 <tomek286@yahoo.com>
2023-04-24 12:10:51 +0200

    commit3

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -18,32 +18,54 @@ const CreatePost = () => {
     const generateImage = async () => {
         if (form.prompt) {
             try {
-              setGeneratingImg(true);
-              const response = await fetch('http://localhost:8080/api/v1/dalle', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  prompt: form.prompt,
-                }),
-              });
-      
-              const data = await response.json();
-              setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+                setGeneratingImg(true);
+                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        prompt: form.prompt,
+                    }),
+                });
+
+                const data = await response.json();
+                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
             } catch (err) {
-              alert(err);
+                alert(err);
             } finally {
-              setGeneratingImg(false);
+                setGeneratingImg(false);
             }
-          } else {
+        } else {
             alert('Please provide proper prompt');
+        }
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (form.prompt && form.photo) {
+          setLoading(true);
+          try {
+            const response = await fetch('http://localhost:8080/api/v1/post', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ ...form }),
+            });
+    
+            await response.json();
+            alert('Success');
+            navigate('/');
+          } catch (err) {
+            alert(err);
+          } finally {
+            setLoading(false);
           }
-    }
-
-    const handleSubmit = () => {
-
-    }
+        } else {
+          alert('Please generate an image with proper details');
+        }
+      };
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -52,7 +74,7 @@ const CreatePost = () => {
     const handleSupriseMe = () => {
         const randomPrompt = getRandomPrompt(form.prompt);
         setForm({ ...form, prompt: randomPrompt });
-     
+
     }
 
     return (
@@ -106,9 +128,18 @@ const CreatePost = () => {
                 </div>
                 <div className="mt-5 flex gap-5">
                     <button type="button" onClick={generateImage}
-                    className='text-white bg-green-700 font-medium rounded-md text-sm
+                        className='text-white bg-green-700 font-medium rounded-md text-sm
                     w-full sm:w-auto px-5 py-2.5 text-center'>
                         {generatingImg ? 'Generating...' : 'Generate Image'}
+                    </button>
+                </div>
+                <div className="mt-10">
+                    <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+                    <button
+                        type="submit"
+                        className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {loading ? 'Sharing...' : 'Share with the Community'}
                     </button>
                 </div>
             </form>
